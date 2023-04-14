4f392af78166136d13b00f3e620c15c5844aacb3
bngams <n.boris@devidia.net>
2023-04-14 12:40:47 +0200

    styles on HeaderComponent

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { MATERIAL_MODULES } from '../utils/material.barrel'
 
 @NgModule({
-  declarations: [],
-  imports: [],
   exports: [
     ...MATERIAL_MODULES // <= imports:[MaterialModule]
   ]
