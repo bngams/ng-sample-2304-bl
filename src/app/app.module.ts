1de92ed53815463cead3528d994665788cc1ef82
bngams <n.boris@devidia.net>
2023-04-14 11:52:05 +0200

    install material and use mat tool bar in header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent, MY_TITLE } from './app.component';
 import { ExponentialStrengthPipe } from './pipes/exponential-strength.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { InlineSampleComponent } from './components/inline-sample.component';
+import {MatToolbarModuleù} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+// Array<Modules>
+const MATERIAL_MODULES = [  
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+    //...
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExponentialStrengthPipe
+    ExponentialStrengthPipe,
+    HeaderComponent,
+    InlineSampleComponent
   ],
   // module ng imports
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    ...MATERIAL_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/inline-sample.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline-sample',
+  template: `
+    <p>
+      inline-sample works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class InlineSampleComponent {
+
+}
