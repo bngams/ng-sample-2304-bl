e315d56d1c77e28b37aa16f75d21abb56b756300
bngams <n.boris@devidia.net>
2023-04-14 15:11:18 +0200

    navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TemplateSyntaxComponent } from './pages/template-syntax/template-syntax.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+// Routes <=> alias <=> type  <=> Route[]
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'template-syntax', component: TemplateSyntaxComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
