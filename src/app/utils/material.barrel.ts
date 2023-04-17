31b7752c7f5efd308b67f935488b7921144258a4
bngams <n.boris@devidia.net>
2023-04-17 08:52:48 +0200

    product module structure

--- src/app/utils/material.barrel.ts
+++ src/app/utils/material.barrel.ts
@@ -1,11 +1,18 @@
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 // Array<Modules>
-export const MATERIAL_MODULES = [  
+export const MATERIAL_MODULES = [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
     //...
 ]
