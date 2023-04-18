2dbca1097d804d93ade29e9483f10a0e229a8504
bngams <n.boris@devidia.net>
2023-04-18 11:25:35 +0200

    create env file to handle some env variables like API URL

--- src/app/modules/product/services/product.service.ts
+++ src/app/modules/product/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<any> {
-    return this.http.get('https://dummyjson.com/products');
+    return this.http.get(`${environment.apiBaseUrl}/products`);
   }
 
 }
